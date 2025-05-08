59c3f96b4c9344c808b0b14e3976f358105107b4
Andreas <andreasdrogehansen1@gmail.com>
2025-05-08 14:37:32 +0200

    feat(search): add markers and callouts to map view

--- app/(protected)/(tabs)/search.tsx
+++ app/(protected)/(tabs)/search.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import MapView, {Callout, Marker} from 'react-native-maps';
+import {StyleSheet, View} from 'react-native';
+import {markers} from "@/constants/markers";
+import {Text} from "@/components/ui/text";
 
 export default function SearchScreen() {
     return (
         <View className={"flex-1"}>
-            <MapView style={styles.map} showsUserLocation={true} showsMyLocationButton={true}/>
+            <MapView style={styles.map} showsUserLocation={true} showsMyLocationButton={true}>
+                {markers.map((marker, index) => (
+                    <Marker key={index} coordinate={marker}>
+                        <Callout>
+                            <View className={"m-2"}>
+                                <Text>{marker.name}</Text>
+                            </View>
+                        </Callout>
+                    </Marker>
+                ))}
+            </MapView>
         </View>
     );
 }
---
+++ constants/markers.ts
@@ -0,0 +1,7 @@
+export const markers= [
+    {
+        latitude: 55.39914205065384,
+        longitude: 9.97325982920486,
+        name: "365discount, 5591 Gelsted, Denmark"
+    }
+]
