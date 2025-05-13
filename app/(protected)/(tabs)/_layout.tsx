9a603f2bbccac756310048f5996201fee2d31f00
Andreas <andreasdrogehansen1@gmail.com>
2025-05-13 14:23:41 +0200

    `style: update tabBar active tint color to match theme (#10b981)`

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -34,15 +34,15 @@ export default function TabsLayout() {
                 backgroundColor: theme.headerStyle.backgroundColor,
             },
             headerTintColor: theme.headerTintColor,
-            tabBarActiveTintColor: 'tomato',
+            tabBarActiveTintColor: '#10b981',
             headerShown: false,
             animation: "fade"
         }}>
-            <Tabs.Screen name={"meals/index"} options={{title: "Udforsk", tabBarLabelStyle: { fontSize: 14}, tabBarIcon: (({focused}) => { return <Icon color={focused ? "tomato" : ""} size={"xl"} as={CalendarDaysIcon} />}), headerShown: false}}></Tabs.Screen>
+            <Tabs.Screen name={"meals/index"} options={{title: "Udforsk", tabBarLabelStyle: { fontSize: 14}, tabBarIcon: (({focused}) => { return <Icon color={focused ? "#10b981" : ""} size={"xl"} as={CalendarDaysIcon} />}), headerShown: false}}></Tabs.Screen>
             <Tabs.Screen name={"meals/[id]"} options={{ href: null }} />
-            <Tabs.Screen name={"search"} options={{title: "Søg", tabBarLabelStyle: { fontSize: 14}, tabBarIcon: (({focused}) => { return <Icon color={focused ? "tomato" : ""} size={"xl"} as={SearchIcon} />}), headerShown: false}}></Tabs.Screen>
-            <Tabs.Screen name={"favorites"} options={{title: "Favoritter", tabBarLabelStyle: { fontSize: 14}, tabBarIcon: (({focused}) => { return <Icon color={focused ? "tomato" : ""} size={"xl"} as={FavouriteIcon} />}), headerShown: false}}></Tabs.Screen>
-            <Tabs.Screen name={"profile"} options={{title: "Profil", tabBarLabelStyle: { fontSize: 14}, tabBarIcon: (({focused}) => { return <Icon color={focused ? "tomato" : ""} size={"xl"} as={SettingsIcon} />}), headerShown: false}}></Tabs.Screen>
+            <Tabs.Screen name={"search"} options={{title: "Søg", tabBarLabelStyle: { fontSize: 14}, tabBarIcon: (({focused}) => { return <Icon color={focused ? "#10b981" : ""} size={"xl"} as={SearchIcon} />}), headerShown: false}}></Tabs.Screen>
+            <Tabs.Screen name={"favorites"} options={{title: "Favoritter", tabBarLabelStyle: { fontSize: 14}, tabBarIcon: (({focused}) => { return <Icon color={focused ? "#10b981" : ""} size={"xl"} as={FavouriteIcon} />}), headerShown: false}}></Tabs.Screen>
+            <Tabs.Screen name={"profile"} options={{title: "Profil", tabBarLabelStyle: { fontSize: 14}, tabBarIcon: (({focused}) => { return <Icon color={focused ? "#10b981" : ""} size={"xl"} as={SettingsIcon} />}), headerShown: false}}></Tabs.Screen>
         </Tabs>
     )
 }
