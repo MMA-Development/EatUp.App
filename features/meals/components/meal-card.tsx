caaeefad443c752acb31a6f951f9cf3af7fe3797
sparkmig <301mikkel103@gmail.com>
2025-06-01 15:39:50 +0200

    refactor(meals): clean up MealCard component structure and formatting

--- features/meals/components/meal-card.tsx
+++ features/meals/components/meal-card.tsx
@@ -1,64 +1,67 @@
-import {Card} from "@/components/ui/card"
-import {Heading} from "@/components/ui/heading"
-import {Text} from "@/components/ui/text"
-import {Image} from "@/components/ui/image"
-import {TouchableOpacity, View} from "react-native";
-import {ArrowRightIcon, FavouriteIcon, Icon} from "@/components/ui/icon";
-import {Meal} from "@/features/meals/types";
+import { Card } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
+import { Image } from "@/components/ui/image";
+import { TouchableOpacity, View } from "react-native";
+import { ArrowRightIcon, FavouriteIcon, Icon } from "@/components/ui/icon";
+import { Meal } from "@/features/meals/types";
 import moment from "moment";
-import {router} from "expo-router";
-import {triggerSoftHaptic} from "@/lib/haptics";
+import { router } from "expo-router";
+import { triggerSoftHaptic } from "@/lib/haptics";
+import FavoriteButton from "@/components/ui/favorite-button";
 
 interface MealCardProps {
-    meal: Meal
+  meal: Meal;
 }
 
-export default function MealCard({meal}: MealCardProps) {
+export default function MealCard({ meal }: MealCardProps) {
+  const handlePress = async () => {
+    await triggerSoftHaptic();
+    router.push({
+      pathname: "/(protected)/(tabs)/meals/[id]",
+      params: { id: meal.id },
+    });
+  };
 
-    const handlePress = async () => {
-        await triggerSoftHaptic();
-        router.push({
-            pathname: "/(protected)/(tabs)/meals/[id]",
-            params: { id: meal.id }
-        })
-    };
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <Card
+        variant="elevated"
+        className="border-[1px] relative min-w-96 my-2 mx-2 rounded-2xl p-0 border-background-100"
+      >
+        <Image
+          source={{
+            uri: `https://eatup.blob.core.windows.net${meal.imageUrl}`,
+          }}
+          alt="image"
+          className="w-full h-32 rounded-t-2xl "
+        />
+        <View className="p-4 flex-col">
+          <Heading size="lg" className="text-primary-900">
+            {meal.vendorName}
+          </Heading>
+          <Text size="md" className="text-primary-700">
+            {meal.title}
+          </Text>
 
+          <View className="flex-row justify-between items-center">
+            <View className="flex-row items-center gap-2">
+              <Text size="lg" className="font-bold text-green-600">
+                {meal.price} kr
+              </Text>
+              <Text size="sm" className="line-through text-gray-400">
+                {meal.originalPrice} kr
+              </Text>
+            </View>
+            <Icon className="text-primary-500" as={ArrowRightIcon} />
+          </View>
 
-    return (
-        <TouchableOpacity onPress={handlePress}>
-            <Card variant="elevated"
-                  className="border-[1px] relative min-w-96 my-2 mx-2 rounded-2xl p-0 border-background-100">
-                <Icon
-                    as={FavouriteIcon}
-                    size={"xl"}
-                    className="absolute top-2 right-2 z-10 text-white"
-                />
-
-                <Image
-                    source={{
-                        uri: `https://eatup.blob.core.windows.net${meal.imageUrl}`,
-                    }}
-                    alt="image"
-                    className="w-full h-32 rounded-t-2xl "
-                />
-                <View className="p-4 flex-col">
-                    <Heading size="lg" className="text-primary-900">{meal.vendorName}</Heading>
-                    <Text size="md" className="text-primary-700">{meal.title}</Text>
-
-                    <View className="flex-row justify-between items-center">
-                        <View className="flex-row items-center gap-2">
-                            <Text size="lg" className="font-bold text-green-600">{meal.price} kr</Text>
-                            <Text size="sm" className="line-through text-gray-400">{meal.originalPrice} kr</Text>
-                        </View>
-                        <Icon className="text-primary-500" as={ArrowRightIcon}/>
-                    </View>
-
-                    <Text size="sm" className="text-gray-600 mt-1">
-                        Afhent i
-                        dag {moment(meal.firstAvailablePickup).format("LT")} – {moment(meal.lastAvailablePickup).format("LT")}
-                    </Text>
-                </View>
-            </Card>
-        </TouchableOpacity>
-    )
+          <Text size="sm" className="text-gray-600 mt-1">
+            Afhent i dag {moment(meal.firstAvailablePickup).format("LT")} –{" "}
+            {moment(meal.lastAvailablePickup).format("LT")}
+          </Text>
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
 }
